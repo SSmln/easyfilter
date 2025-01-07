855e6cbe519595099bb17563a23777550af808ea
SSmln <lsm7630@gmail.com>
2025-01-07 21:08:40 +0900

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,266 @@
-import Image from "next/image";
+"use client";
+import React, { useEffect, useState } from "react";
 
-export default function Home() {
+export const Page = () => {
+  const [data, setData] = useState([]);
+
+  const [filterStack, setFilterStack] = useState([
+    { id: 0, title: "announcementType", value: "전체" },
+    { id: 1, title: "announcementStatus", value: "접수중" },
+    { id: 2, title: "departments", value: ["전체"] },
+  ]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const resposne = await fetch("http://localhost:9999/data");
+      const data = await resposne.json();
+      console.log(data);
+      setData(data);
+    }
+    fetchData();
+  }, []);
+
+  // console.log(data);
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div>
+      <FilterComponent
+        filterStack={filterStack}
+        setFilterStack={setFilterStack}
+      />
+      <div className="w-full h-0.5 bg-gray-300 my-6"></div>
+      <h1 className="mt-6">
+        <p>데이터 정보</p>
+        <div className="flex flex-wrap gap-6">
+          <FilteredData data={data} filterStack={filterStack} />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </h1>
     </div>
   );
-}
+};
+
+export default Page;
+
+const FilteredData = ({ data, filterStack }) => {
+  const type = filterStack[0].value;
+  const status = filterStack[1].value;
+  const department = filterStack[2].value;
+
+  const filteredData = data.filter((e) => {
+    return typeFilter(e) && statusFilter(e) && departmentFilter(e);
+  });
+
+  function typeFilter(e) {
+    if (type === "전체") return e;
+    return e.type === type;
+  }
+  function statusFilter(e) {
+    if (status === "전체") return e;
+    return e.status === status;
+  }
+  function departmentFilter(e) {
+    if (department.includes("전체")) return e;
+    return department.includes(e.categories.main);
+  }
+
+  console.log(filteredData);
+  return (
+    <>
+      {filteredData.map((e, id) => (
+        <div key={e.id} className="max-w-sm rounded overflow-hidden shadow-lg">
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">{e.title}</div>
+            <p className="text-gray-700 text-base">
+              <strong>카테고리:</strong> {e.categories.main} {">"}{" "}
+              {e.categories.sub} {">"} {e.categories.detail}
+            </p>
+            <p className="text-gray-700 text-base">
+              <strong>위치:</strong> {e.location}
+            </p>
+            <p className="text-gray-700 text-base">
+              <strong>가격:</strong> {e.price.toLocaleString()}원
+            </p>
+            <p className="text-gray-700 text-base">
+              <strong>평점:</strong> {e.rating}
+            </p>
+            <p className="text-gray-700 text-base">
+              <strong>시설:</strong> {e.facilities.join(", ")}
+            </p>
+            <p className="text-gray-700 text-base">
+              <strong>설명:</strong> {e.description}
+            </p>
+            <p className="text-gray-700 text-base">
+              <strong>날짜:</strong> {e.date}
+            </p>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+
+const FilterComponent = ({ filterStack, setFilterStack }) => {
+  const extractTitles = (data, key) => data[0][key].map((item) => item.title);
+  const filterType = extractTitles(filterData, "announcementType");
+  const filterStatus = extractTitles(filterData, "announcementStatus");
+  const filterDepartment = extractTitles(filterData, "departments");
+
+  function handleFilter(e, title, type) {
+    const findIndex = filterStack.findIndex((t) => t.title === type);
+    if (findIndex === -1) return;
+    if (e.target.checked) {
+      console.log("checked");
+      if (type === "departments") {
+        setFilterStack((prev) => {
+          return prev.map((t) => {
+            if (t.title === type) {
+              return { ...t, value: [...t.value, title] };
+            }
+            return t;
+          });
+        });
+      } else {
+        setFilterStack((prev) => {
+          return prev.map((t) => {
+            if (t.title === type) {
+              return { ...t, value: title };
+            }
+            return t;
+          });
+        });
+      }
+    } else {
+      console.log("not checked");
+      setFilterStack((prev) => {
+        return prev.map((t) => {
+          if (t.title === type) {
+            return { ...t, value: t.value.filter((v) => v !== title) };
+          }
+          return t;
+        });
+      });
+    }
+  }
+
+  return (
+    <div className="space-y-4">
+      <p>공고 유형 필터</p>
+      <div className="flex ">
+        {filterType.map((title, index) => {
+          return (
+            <label key={index} className="w-full flex items-center space-x-2">
+              <input
+                type="radio"
+                name="announcementType"
+                defaultChecked={title === filterType[0]}
+                value={title}
+                onChange={(e) => {
+                  handleFilter(e, title, "announcementType");
+                }}
+              />
+              <span>{title}</span>
+            </label>
+          );
+        })}
+      </div>
+      <p>공고 상태 필터 </p>
+      <div className="flex ">
+        {filterStatus.map((title, index) => {
+          return (
+            <label key={index} className="w-full flex items-center space-x-2">
+              <input
+                type="radio"
+                name="announcementStatus"
+                defaultChecked={title === filterStatus[2]}
+                value={title}
+                onChange={(e) => {
+                  handleFilter(e, title, "announcementStatus");
+                }}
+              />
+              <span>{title}</span>
+            </label>
+          );
+        })}
+      </div>
+      <p>부처 필터</p>
+      <div className="grid grid-cols-4 gap-2">
+        {filterDepartment.map((title, index) => (
+          <label key={index} className="w-full space-x-2">
+            <input
+              type="checkbox"
+              value={title}
+              defaultChecked={title === filterDepartment[0]}
+              onChange={(e) => {
+                handleFilter(e, title, "departments");
+              }}
+            />
+            <span>{title}</span>
+          </label>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+// export default FilterComponent;
+
+const filterData = [
+  {
+    announcementType: [
+      { id: 1, title: "전체" },
+      { id: 2, title: "통합공고" },
+      { id: 3, title: "개별공고" },
+    ],
+    announcementStatus: [
+      { id: 1, title: "전체" },
+      { id: 2, title: "접수예정" },
+      { id: 3, title: "접수중" },
+      { id: 4, title: "마감" },
+    ],
+    departments: [
+      { id: 1, title: "전체" },
+      { id: 2, title: "개인정보보호위원회" },
+      { id: 3, title: "경찰청" },
+      { id: 4, title: "고용노동부" },
+      { id: 5, title: "공정거래위원회" },
+      { id: 6, title: "과학기술정보통신부" },
+      { id: 7, title: "교육부" },
+      { id: 8, title: "국가보훈부" },
+      { id: 9, title: "국가유산청" },
+      { id: 10, title: "국무조정실" },
+      { id: 11, title: "국방부" },
+      { id: 12, title: "국토교통부" },
+      { id: 13, title: "국회" },
+      { id: 14, title: "기상청" },
+      { id: 15, title: "기획재정부" },
+      { id: 16, title: "농림축산식품부" },
+      { id: 17, title: "농촌진흥청" },
+      { id: 18, title: "대통령경호처" },
+      { id: 19, title: "대통령비서실" },
+      { id: 20, title: "문화체육관광부" },
+      { id: 21, title: "방송통신위원회" },
+      { id: 22, title: "방위사업청" },
+      { id: 23, title: "법무부" },
+      { id: 24, title: "법제처" },
+      { id: 25, title: "보건복지부" },
+      { id: 26, title: "산림청" },
+      { id: 27, title: "산업통상자원부" },
+      { id: 28, title: "소방청" },
+      { id: 29, title: "식품의약품안전처" },
+      { id: 30, title: "여성가족부" },
+      { id: 31, title: "외교부" },
+      { id: 32, title: "우주항공청" },
+      { id: 33, title: "원자력안전위원회" },
+      { id: 34, title: "중소벤처기업부" },
+      { id: 35, title: "질병관리청" },
+      { id: 36, title: "통계청" },
+      { id: 37, title: "통일부" },
+      { id: 38, title: "특허청" },
+      { id: 39, title: "해양경찰청" },
+      { id: 40, title: "해양수산부" },
+      { id: 41, title: "행정안전부" },
+      { id: 42, title: "환경부" },
+      { id: 43, title: "다부처" },
+      { id: 44, title: "기타" },
+    ],
+  },
+];
